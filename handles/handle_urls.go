e02d66145bbeb3a0e7151e3228ed362bdd98b34e
bigseed720 <bigseed720@gmail.com>
2025-05-02 12:07:01 -0400

    try to make an ui

--- handles/handle_urls.go
+++ handles/handle_urls.go
@@ -13,7 +13,16 @@ func index(web http.ResponseWriter , req *http.Request){
     fmt.Fprintf(web , ReadPage("assets/index.html"))
 }
 
+func file(web http.ResponseWriter , req *http.Request){
+	fmt.Fprintf(web , ReadPage("assets/index.html"))
+	web.Header().Set("Content-Type","application/x-www-form-urlencoded")
+	req.ParseForm()
+  	fmt.Println(req.Form["search"][0])
+  	fmt.Println(req.Form["search"][0])
+	
+}
 
 func Handle() {
 	http.HandleFunc("/" , index)
+	http.HandleFunc("/search" , file)
 }
